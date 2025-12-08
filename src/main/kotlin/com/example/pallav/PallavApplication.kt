37cc765079611854293de8b896a32ee9c4eb98de
balaji-nirmit <nirmitpratapsingh11@gmail.com>
2025-12-09 01:41:14 +0530

    initial commit

---
+++ src/main/kotlin/com/example/pallav/PallavApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pallav
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PallavApplication
+
+fun main(args: Array<String>) {
+	runApplication<PallavApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/pallav/PallavApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pallav
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PallavApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
